72bbd41aa607bbcbb043037a00e18ef9bb501e97
mag123c <diehreo@gmail.com>
2023-02-07 09:21:06 +0900

    백단 회원 관련 기능 - 로그인, 로그아웃, 회원가입, 개인정보 조회 및 수정, 회원전체조회 회원가입 시 DB PW 암호화 저장, 로그인 시  복호화 하여 비교처리

--- Project_2ND/src/main/java/com/test/test1/user/service/UserService.java
+++ Project_2ND/src/main/java/com/test/test1/user/service/UserService.java
@@ -2,15 +2,18 @@ package com.test.test1.user.service;
 
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.test.test1.user.dto.UserDto;
 
 public interface UserService {
+
 
 	boolean create(UserDto userDto);
 
 	List<UserDto> list();
 
-	String login(UserDto userDto);
+	String login(UserDto userDto, BCryptPasswordEncoder encoder);
 
 	String emailCheck(String email);
 
